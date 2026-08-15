eb6a7b943c61de291a848744210700f047ab032a
Kirill <106469980+waterflane@users.noreply.github.com>
2026-08-15 14:33:52 +0300

    refactor: split shared core from NeoForge platform

---
+++ core/src/main/java/org/waterflane/villager_potential/core/VillagerPotential.java
@@ -0,0 +1,8 @@
+package org.waterflane.villager_potential.core;
+
+public final class VillagerPotential {
+    public static final String MOD_ID = "villager_potential";
+
+    private VillagerPotential() {
+    }
+}
